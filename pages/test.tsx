f1e3a99b3626b14b2db9568e329819ead438f188
TaegeonKim <61930500+Taegon21@users.noreply.github.com>
2025-01-22 00:48:57 +0900

    refector: Refector folder structure (#1)

---
+++ pages/test.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">테스트 페이지입니다</h1>
+    </div>
+  );
+};
+
+export default TestPage;

1712f51cd59a402739d55a842f6259ecd8a5fabd
TaegeonKim <aksen5240@gmail.com>
2025-02-08 04:49:37 +0900

    chore: Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd ../
-mkdir output
-cp -R ./dnd-12th-8-frontend/. ./output/
-cp -Ra ./dnd-12th-8-frontend/.* ./output/ 2>/dev/null || true
-cp -R ./output/. ./dnd-12th-8-frontend/
-cp -Ra ./output/.* ./dnd-12th-8-frontend/ 2>/dev/null || true
+mkdir -p output
+cp -Ra ./dnd-12th-8-frontend/* ./output
+find ./dnd-12th-8-frontend -name ".*" -type f -exec cp --parents {} ./output \;
+find ./dnd-12th-8-frontend -name ".*" -type d -exec cp -R {} ./output \;

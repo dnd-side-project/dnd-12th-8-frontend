7fdf1c4ef2264844c720b8baf3848fef17efaa61
TaegeonKim <aksen5240@gmail.com>
2025-02-09 01:26:05 +0900

    fix: Resolve build error by adding SVG Webpack config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['picsum.photos', 'images.unsplash.com'],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   experimental: {
     turbo: {
       rules: {

1c5f64279ff685fa2030f1bc725f10faf2d71c4c
TaegeonKim <61930500+Taegon21@users.noreply.github.com>
2025-02-09 03:57:52 +0900

    fix: Resolve build error by adding SVG Webpack config (#24)
    * fix: Resolve build error by adding SVG Webpack config
    
    * fix: Modify svgr related next config & remove turbo flag in dev script
    
    ---------
    
    Co-authored-by: rachel490 <ccori12@naver.com>

--- next.config.ts
+++ next.config.ts
@@ -6,15 +6,13 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['picsum.photos', 'images.unsplash.com'],
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 };
 

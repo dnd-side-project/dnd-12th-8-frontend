5636a011e4768143c2ed887114a138dfe3455140
TaegeonKim <aksen5240@gmail.com>
2025-02-17 09:44:32 +0900

    feat: Apply getlayout

--- src/components/@shared/layout/Layout.tsx
+++ src/components/@shared/layout/Layout.tsx
@@ -1,6 +1,7 @@
+import { ReactElement, ReactNode } from 'react';
 import Header from './Header';
 
-function Layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col items-center bg-gray-900 px-4">
       <div className="min-h-screen w-full tablet:max-w-[800px] laptop:max-w-[1040px] desktop:max-w-[1520px]">
@@ -9,6 +10,13 @@ function Layout({ children }: { children: React.ReactNode }) {
       </div>
     </div>
   );
-}
+};
 
-export default Layout;
+const getPageLayout = (page: ReactElement, pathname: string): ReactNode => {
+  if (pathname === '/main') {
+    return page;
+  }
+  return <Layout>{page}</Layout>;
+};
+
+export default getPageLayout;
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import Header from '@/components/@shared/layout/Header';
 import Carousel from '@/components/home/Carousel';
 import RenderContent from '@/components/home/RenderContent';
 import TabSection from '@/components/home/TabSection';
@@ -10,15 +11,19 @@ const MainPage = () => {
   const { tab = 'popular' } = router.query;
 
   return (
-    <div>
-      <div className="relative right-[50%] left-[50%] mx-[-50vw] w-screen">
-        <div className="mt-15 mb-12">
-          <Carousel items={fakeCarouselItems} />
+    <div className="min-h-screen w-full bg-gray-900">
+      <div className="sticky top-0 left-0 z-50 w-full bg-gray-900">
+        <div className="mx-auto max-w-[1200px] px-4 tablet:px-6 laptop:px-8">
+          <Header />
         </div>
       </div>
-
-      <TabSection activeTab={tab as string} />
-      <RenderContent activeTab={tab as string} postcardItems={fakePostCardItems} />
+      <div className="mt-15 mb-12">
+        <Carousel items={fakeCarouselItems} />
+      </div>
+      <div className="mx-auto max-w-[1200px] px-4 tablet:px-6 laptop:px-8">
+        <TabSection activeTab={tab as string} />
+        <RenderContent activeTab={tab as string} postcardItems={fakePostCardItems} />
+      </div>
     </div>
   );
 };

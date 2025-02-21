22d8ba88a7e27e7a6d06a4ff0f08d82dbe5faf60
TaegeonKim <61930500+Taegon21@users.noreply.github.com>
2025-02-22 08:58:04 +0900

    feat: Update feedback result page (#52)
    * feat: Add post card in project result page
    
    * feat: Update dummy in feedback result page
    
    * feat: Finish UI & api of project details page (#53)
    
    * fix: Fix link url
    
    * feat: Add comment api
    
    * feat: Add responsive sidebar
    
    * chore: Add react query devtools
    
    * feat: Add related projects API
    
    * style: Modify grid layout of relatedProjectSection
    
    * hotfix: Fix link overflow issue in project details
    
    * feat: Add pre-quiz UI & API (#55)
    
    * feat: Add worker section in project details
    
    * style: Modify mobile breakpoint
    
    * feat: Add pre-quiz section API & UI
    
    * chore: Add linking
    
    * hotfix: Fix build issue
    
    * feat: Add post card in project result page
    
    * feat: Update dummy in feedback result page
    
    * feat: Update linking
    
    ---------
    
    Co-authored-by: Rachel Shin <72786354+rachel490@users.noreply.github.com>
    Co-authored-by: rachel490 <ccori12@naver.com>

--- src/pages/statistics/[id].tsx
+++ src/pages/statistics/[id].tsx
@@ -1,17 +1,15 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 // import SmallPostCard from '@/components/@shared/card/post-card/SmallPostCard';
+import { ArrowRightIcon } from '@/assets/icons';
+import SmallPostCard from '@/components/@shared/card/post-card/SmallPostCard';
+import { Icon } from '@/components/@shared/icons/Icon';
 import ABTestStatistics from '@/components/mypage/statistics/ABTestStatistics';
 import LikertStatistics from '@/components/mypage/statistics/LikertStatistics';
 import SelectStatistics from '@/components/mypage/statistics/SelectStatistics';
 import ShortAnswerStatistics from '@/components/mypage/statistics/ShortAnswerStatistics';
 import StatisticsSidebar from '@/components/mypage/statistics/StatisticsSidebar';
-
-// const statisticsFormHeader = {
-//   title: 'AI 추천 시스템 프로젝트',
-//   targetJob: 'developer',
-//   thumbnailImgUrl: 'https://picsum.photos/id/217/100/100',
-//   categoryNames: ['웹', '식음료'],
-// };
+import { useGetProjectDetail } from '@/generated';
 
 const sidebarInfo = {
   totalResponses: 25,
@@ -36,16 +34,10 @@ const shortAnswerStatisticsData = {
   title: '어떤 점을 개선하면 좋을까요?',
   required: true,
   items: [
-    '로딩 속도가 느립니다 로딩 속도가 느립니다 로딩 속도가 느립니다 로딩 속도가 느립니다 로딩 속도가 느립니다 로딩 속도가 느립니다 로딩 속도가 느립니다 로딩 속도가 느립니다',
+    '로딩 속도가 느립니다',
     '모바일 대응이 필요해요',
     '폰트 크기가 좀 작아요',
     '전반적으로 만족스럽습니다',
-    '색상 대비가 부족합니다',
-    '색상 대비가 부족합니다',
-    '색상 대비가 부족합니다',
-    '색상 대비가 부족합니다',
-    '색상 대비가 부족합니다',
-    '색상 대비가 부족합니다',
     '색상 대비가 부족합니다',
   ],
 };
@@ -57,19 +49,7 @@ const ABTestStatisticsData = {
   responseB: 20,
   imageAUrl: 'https://picsum.photos/id/238/200/300',
   imageBUrl: 'https://picsum.photos/id/239/200/300',
-  feedback: [
-    '좋아요',
-    '이런 부분이 이래이래이래서 좋았어요!',
-    '보통이에요',
-    '별로에요',
-    '별로에요',
-    '별로에요',
-    '별로에요',
-    '별로에요',
-    '별로에요',
-    '별로에요',
-    '별로에요',
-  ],
+  feedback: ['좋아요', '이런 부분이 이래이래이래서 좋았어요!', '보통이에요', '별로에요'],
 };
 
 const likertScaleStatisticsData = {
@@ -104,27 +84,44 @@ const likertScaleStatisticsData = {
 const StatisticsPage = () => {
   const router = useRouter();
   const { id } = router.query;
-  console.log('🚀 ~ StatisticsPage ~ id:', id);
+
+  const { data: projectDetail } = useGetProjectDetail(Number(id));
 
   const onClickDelete = () => {
     console.log('삭제하기');
+    alert('준비 중입니다');
   };
 
   const onClickClose = () => {
     console.log('답변 마감하기');
+    alert('준비 중입니다');
   };
+
+  if (!projectDetail) return null;
 
   return (
     <div className="mx-auto flex flex-col gap-8 p-4 pb-55 laptop:flex-row laptop:pb-10">
       <div className="flex-1">
         <div className="mb-8">
-          {/* <SmallPostCard
-            title={statisticsFormHeader.title}
-            targetJob={statisticsFormHeader.targetJob}
-            thumbnailImgUrl={statisticsFormHeader.thumbnailImgUrl}
-            categoryNames={statisticsFormHeader.categoryNames}
-            moveToDetail={`/projects/${id}`}
-          /> */}
+          <div className="flex flex-col border-b border-gray-600 pb-5">
+            <SmallPostCard
+              data={{
+                title: projectDetail?.title ?? '',
+                targetJob: projectDetail?.targetJob ?? '',
+                logoImageUrl: projectDetail?.thumbnailImgUrl ?? '',
+                categoryNames: projectDetail?.platformCategories?.categoryNames ?? ['도서'],
+              }}
+            />
+
+            <div className="flex justify-end">
+              <Link href={`/project/${id}`}>
+                <button className="flex items-center gap-2 rounded-[20px] border-1 border-gray-600 px-4 py-2 font-body2 text-gray-200">
+                  <p>프로젝트 상세보기</p>
+                  <Icon icon={ArrowRightIcon} color="currentColor" />
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
         <div className="flex flex-col gap-8">
           <LikertStatistics {...likertScaleStatisticsData} />
